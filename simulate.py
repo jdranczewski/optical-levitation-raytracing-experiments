e15a18bab514db8aa947212dcece0deccee600e4
Jakub Dranczewski <domdlapsa@gmail.com>
2020-07-27 13:54:05 +0200

    A basic implementation of variables

--- simulate.py
+++ simulate.py
@@ -27,21 +27,48 @@ def derivatives(t, state, forces, mass):
 def main():
     with open("config.yaml", 'r') as f:
         config = yaml.safe_load(f)
-    sim_params = config["params"]
 
-    forces = []
-    for force in config["forces"]:
-        m = import_module("forces."+force["type"])
-        forces.append(m.factory(config, force["params"]))
-    if not len(forces):
-        raise Exception("No forces were defined.")
+    if config["variables"] is not None:
+        var_steps = config["variables"]["steps"]
+        variables = config["variables"]["vars"]
 
-    times = np.linspace(sim_params["start"], sim_params["end"], sim_params["steps"])
-    res = odeint(derivatives, sim_params["initial-conditions"], times, args=(forces, 1), tfirst=True)
+        for vs in range(var_steps):
+            with open("config.yaml", 'r') as f:
+                text = f.read()
+                for var in variables:
+                    val = var["start"] + (var["end"]-var["start"])*vs/var_steps
+                    text = text.replace("__{}__".format(var["name"]), str(val))
+                    print(var["name"])
+                config = yaml.safe_load(text)
+            sim_params = config["params"]
+            forces = []
+            for force in config["forces"]:
+                m = import_module("forces." + force["type"])
+                forces.append(m.factory(config, force["params"]))
+            if not len(forces):
+                raise Exception("No forces were defined.")
 
-    fig, ax = plt.subplots()
-    ax.plot(res[:,0], res[:,1], "o-", ms=3)
-    plt.show()
+            times = np.linspace(sim_params["start"], sim_params["end"], int(sim_params["steps"]))
+            res = odeint(derivatives, sim_params["initial-conditions"], times, args=(forces, 1), tfirst=True)
+            fig, ax = plt.subplots()
+            ax.plot(res[:, 0], res[:, 1], "o-", ms=3)
+            plt.show()
+
+    else:
+        sim_params = config["params"]
+        forces = []
+        for force in config["forces"]:
+            m = import_module("forces."+force["type"])
+            forces.append(m.factory(config, force["params"]))
+        if not len(forces):
+            raise Exception("No forces were defined.")
+
+        times = np.linspace(sim_params["start"], sim_params["end"], sim_params["steps"])
+        res = odeint(derivatives, sim_params["initial-conditions"], times, args=(forces, 1), tfirst=True)
+        fig, ax = plt.subplots()
+        ax.plot(res[:, 0], res[:, 1], "o-", ms=3)
+        plt.show()
+
 
 
 if __name__ == "__main__":
